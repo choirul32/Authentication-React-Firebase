b3c19edd95994327ec8d01ffe99c2181e85927c6
choirul32anwar <choirul32anwar@gmail.com>
2022-03-15 02:11:30 +0700

    handle async

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import {useRef} from "react";
-import { Form, Button, Card } from 'react-bootstrap';
+import React, {useRef,useState} from "react";
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import {signup, useAuth} from "../firebase";
 
 export default function Signup(){
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Passwords do not match')
+        }
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value);
+        } catch {
+            setError("Failed to create an account");
+        }
+        setLoading(false)
+    }
     return(
         <Card>
             <Card.Body>
                 <h2 className='text-center mb-4'>Sign Up</h2>
-                <Form>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
                     <Form.Group id='email'>
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" ref={emailRef} required></Form.Control>
